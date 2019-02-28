cc6b28710dbafa34b9e5eeeea75873b33e65f0f5
Christoph Gruener <christophgruener@me.com>
2019-02-28 18:20:36 +0000

    Done.

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,3 +1,36 @@
 class Doctor
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(patient, date)
+    Appointment.new(date, patient, self)
+  end
+
+  def appointments
+    all_appointments = []
+    Appointment.all.each do |appointment|
+      if appointment.doctor == self
+        all_appointments << appointment
+      end
+    end
+    all_appointments
+  end
+
+  def patients
+    all_patients = []
+    # binding.pry
+    self.appointments.each {|appointment| all_patients << appointment.patient}
+    all_patients
+  end
+
 
 end
