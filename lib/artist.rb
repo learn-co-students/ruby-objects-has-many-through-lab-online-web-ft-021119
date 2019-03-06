a555df133f85d24adaae9e204271132490cd4a7e
Burak Kocak <burakkocak884@ghotmail.com>
2019-03-06 12:44:15 -0500

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,52 @@
+require 'pry'
+class Artist
+
+	attr_accessor :name, :artist, :genre
+
+
+
+	@@all = []
+
+		def initialize(name)
+			@name = name
+		 	
+			@@all << self
+			#binding.pry
+
+		end
+
+
+
+
+				def self.all
+					@@all 
+				end
+
+				def new_song(name, genre)
+						song = Song.new(name, self, genre)
+						#binding.pry
+
+
+
+
+				end
+
+				
+				def songs
+					#binding.pry
+					Song.all.select do |songs|
+						songs.artist == self
+					end
+
+				end
+				def genres
+					Song.all.collect do |songs|
+						#binding.pry
+						songs.genre
+					end
+
+				end
+
+			
+
+		end
